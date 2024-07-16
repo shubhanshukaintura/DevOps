c0fa0df23a043f57b8f7545af6ef8d325027eb45
Shubhanshu Kaintura <shubhanshukaintura2@gmail.com>
2024-07-16 11:21:22 +0530

    new demoMaven

--- demoMaven/src/main/java/com/example/demo/test.java
+++ demoMaven/src/main/java/com/example/demo/test.java
@@ -1,5 +1,7 @@
+package com.example.demo;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
     }
 }
